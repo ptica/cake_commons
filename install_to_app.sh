49c8dba426431663283ddc96d45f3d0d166bfa63
Jan Ptacek <jan.ptacek@gmail.com>
2011-03-03 17:35:43 +0100

    fullcalendar stuff

--- install_to_app.sh
+++ install_to_app.sh
@@ -5,8 +5,8 @@ if [ -z "$1" -o ! -e "$1" ]; then echo "usage: $0 <app_root_dir>"; exit; fi
 #
 # symlink dirs content where they belong in a cake app
 #
-src=( behaviors components  helpers layouts vendors views js      shells)
-dst=( models    controllers views   views   '.'      '.'  webroot vendors) 
+src=( behaviors components  helpers layouts vendors views js      shells  css     )
+dst=( models    controllers views   views   '.'      '.'  webroot vendors webroot ) 
 count=${#src[@]}
 i=0
 script_dir=$(./canon_path.sh .) 
