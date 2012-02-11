3be26bd762fc97c5ad2dacb8aa5302276c68b546
jan.ptacek <jan.ptacek@gmail.com>
2012-02-11 11:15:20 +0100

    darwin branch for canon_path(pythony)

--- canon_path.sh
+++ canon_path.sh
@@ -2,10 +2,14 @@
 
 system=`uname`;
 
-if [ $system = 'FreeBSD' ];
+if [ $system = 'FreeBSD' ]
 then
-	echo $(realpath $1);
+	echo $(realpath $1)
+elif [ $system = 'Darwin' ]
+then
+	#alias realpath="python -c 'import os, sys; print os.path.realpath(sys.argv[1])'"
+	echo $(python -c 'import os, sys; print os.path.realpath(sys.argv[1])' $1)
 else
-	echo $(readlink -f $1);
+	echo $(readlink -f $1)
 fi
 
